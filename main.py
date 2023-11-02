ef86aaf026b8cecd72768a38a1c989ff9c028691
ShaddySait <shady.sheikh@edu.sait.ca>
2023-11-02 15:55:30 -0600

    validated

---
+++ main.py
@@ -0,0 +1,15 @@
+sample="380 339 420 308 448"
+cols=sample.split()
+num_list=[]
+for col in cols:
+    num=int(col)
+    num_list.append(num)
+start= min(num_list)    
+for count in range(start,(max(num_list))):
+    # print(count)
+    if count not in num_list:
+    #     # we have membership
+    #     pass
+    # else:
+    #     #otherwise 
+        print(f"{count} is missing")
